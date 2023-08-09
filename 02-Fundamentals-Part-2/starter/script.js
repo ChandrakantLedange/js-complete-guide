598166a93ecfc0f1f080259595618b3073641cd4
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2023-08-09 23:55:49 +0530

    PTC : function declaration vs expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -25,3 +25,26 @@ console.log(fruitJuice(4, 2)); //juice with 4 apples adn 2 oranges
 
 const freshJuice = fruitJuice(3, 1);
 console.log(freshJuice); //juice with 3 apples adn 1 oranges
+
+//function declaration VS function expression
+//Function Declaration
+// const age = calAge(1994);//46
+function calAge(birthAge) {
+  return 2040 - birthAge;
+}
+const age = calAge(1994);
+console.log("age:", age); //46
+
+// Note: function declaration can call before function declare/defined.
+
+//function Expression
+// console.log(currAge(1994)); //will get error
+
+const currAge = function (birthAge) {
+  return 2040 - birthAge;
+};
+
+console.log(currAge(1994)); //46
+
+//Note : function expression can not call before declare or defined
+//will get error
